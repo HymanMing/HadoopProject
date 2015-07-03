146b4c3347adb460606d64e9640c7a7697925b00
Alvin Gui <ahqsgmm@gmail.com>
2015-07-03 21:24:56 +0800

    MapReduce小案例：个人改进的MapReduce模板 TopK 数据去重 数据排序 数据统计 基站日志分析

--- src/main/java/cn/hduhadoop/hadoop/mapreduce/WordCountMapReduce.java
+++ src/main/java/cn/hduhadoop/hadoop/mapreduce/WordCountMapReduce.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -15,6 +16,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCountMapReduce {
 
+	public static enum WORDCOUNT_COUNTERS{
+		MAP_INPUT_RECORD_COUNTER
+	}
 	public static class MyMapper extends
 			Mapper<LongWritable, Text, Text, LongWritable> {
 		private Text mapOutPutKey = new Text();
@@ -23,6 +27,8 @@ public class WordCountMapReduce {
 		@Override
 		protected void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
+			Counter counter = context.getCounter(WORDCOUNT_COUNTERS.MAP_INPUT_RECORD_COUNTER);
+			counter.increment(1L);
 			String lineValue = value.toString();
 
 			StringTokenizer tokenizer = new StringTokenizer(lineValue);
@@ -55,8 +61,8 @@ public class WordCountMapReduce {
 
 	public static void main(String[] args) throws Exception {
 		args = new String[]{
-				"hdfs://10.1.16.251:8020/user/hyman/mr/input",
-				"hdfs://10.1.16.251:8020/user/hyman/mr/output1"
+				"hdfs://10.1.16.251:8020/user/hyman/mr/wc/input",
+				"hdfs://10.1.16.251:8020/user/hyman/mr/wc/mapoutput"
 		};
 		Configuration conf = new Configuration();
 
@@ -68,7 +74,8 @@ public class WordCountMapReduce {
 		job.setMapperClass(MyMapper.class);
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(LongWritable.class);
-
+		
+		job.setNumReduceTasks(0);
 		job.setReducerClass(MyReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(LongWritable.class);
